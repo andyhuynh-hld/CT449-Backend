ca75e491eca0c98b64440aa016629f50daba33b4
CM24V7X308 <andyhuynh.hld@gmail.com>
2025-11-08 21:50:47 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const contactsRouter = require("./app/routes/contact.route");//Đăng ký route với express app  contact.route.js
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);//Đăng ký route với express app  contact.route.js
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
